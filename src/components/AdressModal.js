35a7b9ffd000cfce8c6de5e10f0439d76c2bd47d
Jagdamba Tripathi <jagdtri2003@gmail.com>
2024-10-26 00:16:49 +0530

    Implemented Current Location Feature

--- src/components/AdressModal.js
+++ src/components/AdressModal.js
@@ -4,6 +4,7 @@ import "../style/adressmodal.css"
 const AddressModal = ({ isOpen, onClose, onSubmit }) => {
   const [address, setAddress] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
+  const [locationLoading,setLocationLoading] = useState(false);
 
   React.useEffect(()=>{
       if(localStorage.getItem('address') && localStorage.getItem('phoneNumber')) {
@@ -19,6 +20,27 @@ const AddressModal = ({ isOpen, onClose, onSubmit }) => {
       localStorage.setItem('phoneNumber', phoneNumber);
       onSubmit({ address, phoneNumber });
       onClose(); 
+    }
+  };
+  const handleCurrentLocation = () => {
+    setLocationLoading(true);
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        const { latitude, longitude } = position.coords;
+        
+        const options = {method: 'GET', headers: {accept: 'application/json'}};
+
+        fetch(`https://us1.locationiq.com/v1/reverse?lat=${latitude}&lon=${longitude}&format=json&key=pk.4cb6822cb40a515d8d8c5f7d02cca2ca`, options)
+          .then(res => res.json())
+          .then(res => setAddress(res.display_name))
+          .catch(err => console.error(err));
+      }, 
+      (error) => {
+        console.error("Error getting location:", error);
+        alert("Unable to retrieve your location.");
+      });
+    } else {
+      alert("Geolocation is not supported by your browser.");
     }
   };
 
@@ -40,6 +62,16 @@ const AddressModal = ({ isOpen, onClose, onSubmit }) => {
               onChange={(e) => setAddress(e.target.value)}
               required
             />
+          </div>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <hr style={{ flex: 1, maxWidth: '48%' }} />
+            <span style={{ padding: '0 10px' }}>OR</span>
+            <hr style={{ flex: 1, maxWidth: '48%' }} />
+          </div>
+          <div style={{ display: 'flex', alignItems: 'center',justifyContent: 'center' }}>
+            <button type='button'  onClick={handleCurrentLocation}
+            disabled={locationLoading}
+            style={{ width: '100%',maxWidth: '250px',padding: '10px 20px',marginBottom:'10px',borderRadius:'5px',backgroundColor:'black',color:'white',cursor:'pointer' }}>Use Current Location &nbsp;<i className="fa-solid fa-location-dot"></i></button> 
           </div>
           <div>
             <label htmlFor="phone">Phone Number:</label>
